4ab2608aec7dca41820bc405c7cb527dc2046bda
Kevin M. White <86124561+Macjutsu@users.noreply.github.com>
2024-02-29 12:43:06 -0500

    Update EraseAllContentsEA.sh

--- EraseAllContentsEA.sh
+++ EraseAllContentsEA.sh
@@ -2,7 +2,11 @@
 # This Jamf Pro Extension Attribute reports if the computer is capable of performing the Erase All Contents And Settings action.
 
 if [[ $(arch) == "arm64" ]]; then # Mac computer with Apple Silicon.
-	echo "<result>True</result>"
+	if [[ $(sw_vers -productVersion | cut -d'.' -f1) -ge 12 ]]; then
+		echo "<result>True</result>"
+	else # Incompatible macOS version.
+		echo "<result>True, incompatible macOS version</result>"
+	fi
 else # Mac computers with Intel.
 	if [[ $(system_profiler SPiBridgeDataType | grep -c 'Apple T2') -gt 0 ]]; then
 		if [[ $(sw_vers -productVersion | cut -d'.' -f1) -ge 12 ]]; then
